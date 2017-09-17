59a5dd62b3cfa1f8283f3c88d7c8c83eafca9f43
yohanstra <jreinstra@gmail.com>
2017-09-16 21:52:21 -0400

    Add url routing / multiple pages

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
-var app = angular.module("oceansWeb", []); 
+var app = angular.module("oceansWeb", ["ngRoute"]);
 
-app.controller("mainStats", function($scope, $timeout, $interval) {
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "templates/home.html",
+        controller : "HomeController"
+    })
+    .when("/voter", {
+        templateUrl : "templates/voter.html",
+        controller : "VoterController"
+    })
+    .when("/election", {
+        templateUrl : "templates/election.html",
+        controller : "ElectionController"
+    });
+});
+
+
+app.controller("HomeController", function($scope, $timeout, $interval) {
     $scope.electionStatus = "Not running";
     $scope.numVoters = 4024553;
     $scope.numVotes = 0;
     $timeout(function() {
        $scope.electionStatus = "Running";
-        console.log("running");
         $interval(function() {
             $scope.numVotes += 1000 + parseInt(Math.random() * 2000);
         }, 1000);
     }, 2000);
+});
+
+app.controller("VoterController", function($scope, $timeout, $interval) {
+    $scope.foo = "bar"
+});
+
+app.controller("ElectionController", function($scope, $timeout, $interval) {
+    $scope.foo = "bar"
 });
