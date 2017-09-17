13ff3efdfff6a1fb084b8a99cc4e57fbab5caac1
yohanstra <jreinstra@gmail.com>
2017-09-17 05:19:19 -0400

    Finish algorithm

--- rank.js
+++ rank.js
@@ -46,25 +46,45 @@ var trim_candidates = function(address_to_votes, candidate_to_votes) {
         var done = false;
         while(!done) {
             var all_equal = true;
-            for(var i = 1; i < current_candidates.length; i++) {
-                if(current_candidates[i-1][0] != current_candidates[i][0]) all_equal = false;
+            var lengths = [];
+            for(var i in current_candidates) {
+                if(current_candidates[i]) lengths.push(current_candidates[i].length);
             }
+            console.log(lengths);
+            for(var i = 0; i < lengths.length; i++) {
+                for(var j = 0; j < lengths.length; j++) {
+                    if(lengths[i] != lengths[j]) all_equal = false;
+                }
+            }
+            console.log("all equal:", all_equal);
             if(all_equal) {
                 done = true;
             }
             else {
                 var lowest_index = 0;
                 for(var index in current_candidates) {
-                    if(current_candidates[index][0] < current_candidates[lowest_index][0]) {
+                    if(!current_candidates[index]) continue;
+                    if(current_candidates[index].length < current_candidates[lowest_index].length) {
                         lowest_index = index;
                     }
                 }
-                console.log(current_candidates, "about to remove:", lowest_index)
-                current_candidates.splice(lowest_index, 1);
+                console.log(current_candidates, "about to remove:", lowest_index);
+                for(var address_index in current_candidates[lowest_index]) {
+                    var address = current_candidates[lowest_index][address_index];
+                    console.log("before removal:", address_to_votes[address][question_id]);
+                    var next_candidate_index = address_to_votes[address][question_id][0];
+                    while(next_candidate_index == lowest_index || !current_candidates[next_candidate_index]) {
+                        address_to_votes[address][question_id].splice(0, 1);
+                        console.log("remaining votes for ", address, ":", address_to_votes[address][question_id]);
+                        next_candidate_index = address_to_votes[address][question_id][0];
+                    }
+                    current_candidates[next_candidate_index].push(address);
+                }
+                current_candidates[lowest_index] = null;
             }
         }
     }
-    console.log("result:", candidate_to_votes);
+    console.log("Done!  Candidates remaining are:", candidate_to_votes);
 };
 
 var massage_data = function() {
@@ -82,9 +102,6 @@ var massage_data = function() {
         // console.log("1");
         for(var candidate_number in candidates) {
             candidate_to_votes[question_id][candidate_number] = [];
-            for(var preference_number = 0; preference_number < candidates.length; preference_number++) {
-                candidate_to_votes[question_id][candidate_number][preference_number] = 0;
-            }
         }
         // console.log("2");
         for(var i in voters) {
@@ -103,7 +120,9 @@ var massage_data = function() {
                         // console.log("a");
                         address_to_votes[address][question_number][preference_number] = candidate_number;
                         // console.log("b");
-                        candidate_to_votes[question_number][candidate_number][preference_number]++;
+                        if(preference_number == 1) {
+                            candidate_to_votes[question_number][candidate_number].push(address);
+                        }
                         // console.log("result:", result, "candidate number:", candidate_number, address_to_votes, candidate_to_votes);
                     }
                     shitty_semaphore--;
